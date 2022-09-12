e93b7c2e11cca3a8c02359df00af0e20f7ac1eb8
Jack Upton <acidplaystf2@gmail.com>
2022-09-12 13:24:26 +1200

    Updated description

--- jack-upton-design/src/desktop/header-section/header-section.js
+++ jack-upton-design/src/desktop/header-section/header-section.js
@@ -41,7 +41,7 @@ class HeaderSection extends Component {
                   </Link>
                 </div>
                 <div className='Header-Text-Section-AboutMe'>
-                  I’m a designer & front-end developer, specialising in bridging aesthetics and code. I most recently finished up my contract working with Cenareo in Toulouse, France, aiding with creating a design system for their content management system. With a degree in Design Innovation from VUW & 2 years of professional experience, I am competent in a wide variety of domains including user experience design, visual design, illustration, and front end code.
+                  I’m a digital designer & front-end developer, specialising in bridging aesthetics and code. I most recently finished working with Waste Management in Auckland, employed as a Digital Solutions Designer. I have a degree in Design Innovation from Victoria University of Wellington & 4+ years of professional experience. I have extensive knowledge of many facets of web design, including UX, UI, design systems, front-end code, and user research.
                 </div>
               </div>
               <Fade duration={4000}>
